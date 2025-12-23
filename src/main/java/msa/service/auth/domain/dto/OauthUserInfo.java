1a56d4c11bcbde541ee8a9b7d3c430d6cbc8e54a
BieNew22 <newbie11004@gmail.com>
2025-12-23 17:26:08 +0900

    feat: add oauth strategy and resolver

--- src/main/java/msa/service/auth/domain/dto/OauthUserInfo.java
+++ src/main/java/msa/service/auth/domain/dto/OauthUserInfo.java
@@ -3,4 +3,7 @@ package msa.service.auth.domain.dto;
 public record OauthUserInfo(
         String userEmail
 ) {
+    public static OauthUserInfo from(String email) {
+        return new OauthUserInfo(email);
+    }
 }
--- src/main/java/msa/service/auth/service/AuthService.java
+++ src/main/java/msa/service/auth/service/AuthService.java
@@ -2,19 +2,28 @@ package msa.service.auth.service;
 
 
 import lombok.RequiredArgsConstructor;
+import msa.service.auth.domain.dto.OauthUserInfo;
+import msa.service.auth.infra.oauth.OauthLoginStrategy;
+import msa.service.auth.infra.oauth.OauthLoginStrategyResolver;
 import msa.service.auth.repository.AccountRepository;
 import msa.service.auth.service.request.OAuthRequest;
 import msa.service.auth.service.response.LoginResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
     private final AccountRepository accountRepository;
+    private final OauthLoginStrategyResolver oauthLoginStrategyResolver;
 
+    @Transactional
     public LoginResponse oAuthLogin(OAuthRequest request) {
+        OauthLoginStrategy strategy = oauthLoginStrategyResolver.resolve(request.type());
 
-        return LoginResponse.from("a", "b");
+        OauthUserInfo userInfo = strategy.verifyAndExtract(request.authToken());
+
+        return LoginResponse.from(userInfo.userEmail(), "b");
     }
 
 }
